23505d65bdff39f9f2d6b65970e75d69b6b54b05
Muhammad Daffi Erick Affan <Daffi.affan2709@gmail.com>
2024-05-09 01:26:31 +0700

    misahin composable dari activity

--- app/src/main/java/org/d3if0146/assessment2mobpro/MainActivity.kt
+++ app/src/main/java/org/d3if0146/assessment2mobpro/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import org.d3if0146.assessment2mobpro.ui.screen.MainScreen
 import org.d3if0146.assessment2mobpro.ui.theme.Assessment2MobProTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,29 +16,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             Assessment2MobProTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface( modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    MainScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Assessment2MobProTheme {
-        Greeting("Android")
     }
 }
